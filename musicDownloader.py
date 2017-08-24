2cd04a3ae70f72746469b7053e4b95b1fd5680e8
Mark Babatunde <markbabatunde@pitt.edu>
2017-08-24 15:38:10 -0400

    Fixing spaces and indentation

--- musicDownloader.py
+++ musicDownloader.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-__author__ == "Mark Babatunde"
+__author__ = "Mark Babatunde"
 
 import urllib.request
 import json
 
 def adultSwim():
-	with urllib.request.urlopen("https://singles2017.s3.amazonaws.com/data/data.json") as url:
-		data = json.loads(url.read().decode())
-		for item in data["singles"]:
-			print (item["title"] + ":" + "\t" + item["artist_name"] + "\t" + item["mp3_file"])
+    with urllib.request.urlopen("https://singles2017.s3.amazonaws.com/data/data.json") as url:
+        data = json.loads(url.read().decode())
+        for item in data["singles"]:
+            print (item["title"] + ":" + "\t" + item["artist_name"] + "\t" + item["mp3_file"])
 if __name__ == "__main__":
-	adultSwim()
+    adultSwim()
 
